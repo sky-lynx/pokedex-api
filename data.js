3c2724a8f2a096107ccf7610432e0d4be3f63d88
Aaron <aaronjihoo@gmail.com>
2024-09-21 20:17:38 -0700

    Added full data for Bulbasaur

--- data.js
+++ data.js
@@ -1,7 +1,22 @@
-const pokemonData = [
-  { id: 1, name: 'Bulbasaur', type: 'Grass', ability: 'Overgrow' },
-  { id: 2, name: 'Charmander', type: 'Fire', ability: 'Blaze' },
-  { id: 3, name: 'Squirtle', type: 'Water', ability: 'Torrent' },
-];
+module.exports = [
 
-module.exports = pokemonData;
+  { 
+    id: 1,
+    name: 'Bulbasaur',
+    type: ['Grass', 'Poison'],
+    abilities: ['Overgrow'],
+    hability: ['Chlorophyll'],
+    baseStats: [45, 49, 49, 65, 65, 45],
+    classification: 'Seed',
+    catchRate: 45,
+    eggGroup: ['Monster', 'Grass'],
+    eggCycle: 20,
+    levelRate: ['MediumSlow'],
+    height: 0.7,
+    weight: 6.9,
+    baseFriendship: 50,
+    yield: [64, 0, 0,	0, 1, 0, 0, 1],
+    gender: [87.5, 12.5]
+  },
+
+];
